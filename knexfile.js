a150fb982757dd2ecc2fb37e17abdcc7f4f09dfd
Stanimir Monev <58228578+StanMonev@users.noreply.github.com>
2024-08-02 16:45:27 +0200

    Fixed knex production directory.

--- knexfile.js
+++ knexfile.js
@@ -15,10 +15,10 @@ module.exports = {
         client: 'postgresql',
         connection: process.env.DATABASE_URL,
         migrations: {
-            directory: './migrations'
+            directory: './db/migrations'
         },
         seeds: {
-            directory: './seeds'
+            directory: './db/seeds'
         }
     }
 };
